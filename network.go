08d103f5e1f12ab880f72848090f79621eebc46e
Samuel Watson <samuelwatson@outlook.com.au>
2025-02-20 22:03:49 +1100

    updated InitializeWithLayers

--- network.go
+++ network.go
@@ -52,7 +52,6 @@ func NewPhaseWithLayers(layers []int, hiddenAct, outputAct string) *Phase {
 // InitializeWithLayers resets the Phase and builds a fully connected network
 // with the specified layers, hidden activation, and output activation.
 func (bp *Phase) InitializeWithLayers(layers []int, hiddenAct, outputAct string) {
-	// Reset existing state to ensure a clean initialization
 	bp.Neurons = make(map[int]*Neuron)
 	bp.InputNodes = []int{}
 	bp.OutputNodes = []int{}
@@ -60,16 +59,19 @@ func (bp *Phase) InitializeWithLayers(layers []int, hiddenAct, outputAct string)
 	bp.ScalarActivationMap = scalarActivationFunctions
 	bp.InitializeActivationFunctions()
 
-	neuronID := 0
+	if bp.Debug {
+		fmt.Printf("Initializing network with layers: %v, hiddenAct: %s, outputAct: %s\n", layers, hiddenAct, outputAct)
+	}
 
-	// Add input neurons
+	neuronID := 0
 	for i := 0; i < layers[0]; i++ {
 		bp.Neurons[neuronID] = &Neuron{ID: neuronID, Type: "input"}
 		bp.InputNodes = append(bp.InputNodes, neuronID)
+		if bp.Debug {
+			fmt.Printf("Added input neuron %d\n", neuronID)
+		}
 		neuronID++
 	}
-
-	// Add hidden and output layers
 	prevLayerStart := 0
 	for l := 1; l < len(layers); l++ {
 		currentLayerStart := neuronID
@@ -84,7 +86,6 @@ func (bp *Phase) InitializeWithLayers(layers []int, hiddenAct, outputAct string)
 				Activation: act,
 				Bias:       rand.Float64()*0.1 - 0.05,
 			}
-			// Connect to all neurons in the previous layer
 			for j := prevLayerStart; j < currentLayerStart; j++ {
 				weight := rand.Float64()*2 - 1
 				bp.Neurons[j].Connections = append(bp.Neurons[j].Connections, []float64{float64(neuronID), weight})
@@ -93,9 +94,15 @@ func (bp *Phase) InitializeWithLayers(layers []int, hiddenAct, outputAct string)
 			if l == len(layers)-1 {
 				bp.OutputNodes = append(bp.OutputNodes, neuronID)
 			}
+			if bp.Debug {
+				fmt.Printf("Added neuron %d (Type: %s, Activation: %s)\n", neuronID, bp.Neurons[neuronID].Type, act)
+			}
 			neuronID++
 		}
 		prevLayerStart = currentLayerStart
+	}
+	if bp.Debug {
+		fmt.Printf("Initialization complete. Neurons: %d, InputNodes: %v, OutputNodes: %v\n", len(bp.Neurons), bp.InputNodes, bp.OutputNodes)
 	}
 }
 
